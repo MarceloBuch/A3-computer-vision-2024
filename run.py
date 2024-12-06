8c8ce9c2739b9fc74f225d857df7b6f22d48ea39
Marcelo Buchalowicz <marcelo.buch@hotmail.com>
2024-12-06 13:21:02 -0300

    API settings finalized

--- run.py
+++ run.py
@@ -1,3 +1,5 @@
+from flask import jsonify, render_template
+from app.models import Bus
 from app import create_app
 from app.processor import process_video
 import threading
@@ -7,6 +9,17 @@ app = create_app()
 
 #ID do ônibus
 id_bus = 1
+
+# Definindo rotas da API
+@app.route("/index")
+def index():
+    buses = Bus.query.all()
+    return render_template("./index.html", buses=buses)
+
+@app.route("/list", methods=["GET"])
+def list_bus_data():
+    buses = Bus.query.all()
+    return jsonify([bus.to_dict() for bus in buses])
 
 # Inicia o processamento contínuo em um thread
 thread = threading.Thread(target=process_video, args=(id_bus, app))
